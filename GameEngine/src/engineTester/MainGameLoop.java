ebd4df26d5186a2dd8bb5501bf0cf83e93f99fc9
csalad-pc <csalad-pc@192.168.0.7>
2017-05-22 17:49:50 +0200

    Created Textured Model

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -2,19 +2,22 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 		
 		DisplayManager.createDisplay();
-		
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
 		float[] vertices = {
 				  -0.5f, 0.5f, 0,
@@ -27,16 +30,27 @@ public class MainGameLoop {
 				  0,1,3,
 				  3,1,2
 				};
+				
+				float[] textureCoords = {
+						0,0,
+						0,1,
+						1,1,
+						1,0
+				};
 		
-		RawModel model = loader.loadToVAO(vertices, indices);
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		while(!Display.isCloseRequested()){
-			renderer.prepare();
 			//game logic
-			renderer.render(model);
+			renderer.prepare();
+			shader.start();
+			renderer.render(texturedModel);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
-		
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
