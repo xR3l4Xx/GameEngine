deafd7930316b2f4dec7829a42dd59dc8778b750
ddavid <ddavid@192.168.0.19>
2017-05-22 13:14:34 +0200

    Update

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -3,6 +3,9 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -10,13 +13,31 @@ public class MainGameLoop {
 		
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				  -0.5f, 0.5f, 0,
+				  -0.5f, -0.5f, 0,
+				  0.5f, -0.5f, 0,
+				  0.5f, 0.5f, 0f
+				};
+				  
+				int[] indices = {
+				  0,1,3,
+				  3,1,2
+				};
+		
+		RawModel model = loader.loadToVAO(vertices, indices);
+		
 		while(!Display.isCloseRequested()){
-			
+			renderer.prepare();
 			//game logic
-			//render
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
 		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
 
