9b0d0fceac422b6a5e4dc544957494daa6ce1e4f
csalad-pc <csalad-pc@192.168.0.7>
2017-06-01 23:32:51 +0200

    First Commit

---
+++ EvolutionaryCanon/src/EvolutionaryCanon.java
@@ -0,0 +1,9 @@
+
+public class EvolutionaryCanon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ EvolutionaryCanon/src/Window.java
@@ -0,0 +1,5 @@
+import javax.swing.JPanel;
+
+public class Window extends JPanel{
+
+}
